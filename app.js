716361e26a84c0357909f0d9fbcd434187844661
TimAaronSharp <timothyaaron.sharp@gmail.com>
2025-02-17 17:44:09 -0700

    Project complete

--- app.js
+++ app.js
@@ -9,27 +9,38 @@ let awayScore = 0;
 // SECTION FUNCTIONS
 console.log(homeScore)
 function scoreChange(points, team) {
-  // console.log('Button works bruh');
-  // const newScore = document.getElementById("home-score")
   if (team == "home" && homeScore <= 0 && points == -1) {
     homeScore = 0
     console.log("Can't go lower than 0");
+    drawScore(team)
     return
   } else if (team == 'home') {
     homeScore += points
     console.log('homeScore ' + homeScore);
+    drawScore(team)
     return
   }
   if (team == "away" && awayScore <= 0 && points == -1) {
     awayScore = 0
     console.log("Can't go lower than 0");
+    drawScore(team)
     return
   } else if (team == 'away') {
     awayScore += points
     console.log('awayScore' + awayScore);
+    drawScore(team)
     return
   }
+}
 
+function drawScore(team) {
+  if (team == 'home') {
+    const homeScoreElem = document.getElementById("home-score")
+    homeScoreElem.innerText = homeScore
+  } else {
+    const awayScoreElem = document.getElementById("away-score")
+    awayScoreElem.innerText = awayScore
+  }
 }
 
 // !SECTION FUNCTIONS
