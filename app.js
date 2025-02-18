9e12e94496b9ab2133378111b508a3ea86d89e5c
TimAaronSharp <timothyaaron.sharp@gmail.com>
2025-02-17 17:23:13 -0700

    Score btn functions change scores - prevents score going below 0

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-console.log("Did this work?");
+// SECTION VARIABLES
+
+let homeScore = 0;
+
+let awayScore = 0;
+
+// !SECTION VARIABLES
+
+// SECTION FUNCTIONS
+console.log(homeScore)
+function scoreChange(points, team) {
+  // console.log('Button works bruh');
+  // const newScore = document.getElementById("home-score")
+  if (team == "home" && homeScore <= 0 && points == -1) {
+    homeScore = 0
+    console.log("Can't go lower than 0");
+    return
+  } else if (team == 'home') {
+    homeScore += points
+    console.log('homeScore ' + homeScore);
+    return
+  }
+  if (team == "away" && awayScore <= 0 && points == -1) {
+    awayScore = 0
+    console.log("Can't go lower than 0");
+    return
+  } else if (team == 'away') {
+    awayScore += points
+    console.log('awayScore' + awayScore);
+    return
+  }
+
+}
+
+// !SECTION FUNCTIONS
